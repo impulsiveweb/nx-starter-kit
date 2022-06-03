f9e7bda7aed9f826d1bc966236d7d06e4241d021
Sheetal Kumar <sheetal@impulsiveweb.com>
2022-06-03 20:22:43 +0530

    Update

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,10 +3,9 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { APP_FILTER } from "@nestjs/core";
 import { resolve } from "path";
 
-import { LoggerModule } from "@logger/logger.module";
-
+import { LoggerModule } from "./logger/logger.module";
 import { DatabaseModule } from "@database/database.module";
-import { JwtModule } from "@jwt/jwt.module";
+import { JwtModule } from "./jwt/jwt.module";
 import { ExceptionFilter } from "./exception.filter";
 import { AuthModule } from "./modules/auth/auth.module";
 import { DashboardModule } from "./modules/dashboard/dashboard.module";
