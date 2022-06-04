86a9a9e04d1592da4ba66022e9d1d254ce67dd99
Sheetal Kumar <sheetal@impulsiveweb.com>
2022-06-04 13:24:41 +0530

    Image Compression Done

--- apps/api/src/modules/files/files.module.ts
+++ apps/api/src/modules/files/files.module.ts
@@ -1,14 +1,8 @@
 import { Module } from "@nestjs/common";
 import { FilesService } from "./files.service";
 import { FilesController } from "./files.controller";
-import { AwsSdkModule } from "nest-aws-sdk";
-import { Credentials } from "aws-sdk";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-
 @Module({
-  imports: [
-    
-  ],
+  imports: [],
   providers: [FilesService],
   controllers: [FilesController]
 })
--- apps/api/src/modules/files/files.service.ts
+++ apps/api/src/modules/files/files.service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@config';
 import { User } from '@database/user.model';
 import { getFilterParams } from '@utils/helpers';
 import { S3 } from 'aws-sdk';
-import { InjectAwsService } from 'nest-aws-sdk';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class FilesService {
@@ -11,41 +11,83 @@ export class FilesService {
 
   getS3() {
     return new S3({
-        accessKeyId: this.config.get('S3_ACCESS_KEY_ID'),
-        secretAccessKey: this.config.get('S3_SECRET_ACCESS_KEY'),
+      accessKeyId: this.config.get('S3_ACCESS_KEY_ID'),
+      secretAccessKey: this.config.get('S3_SECRET_ACCESS_KEY'),
     });
   }
 
-  async listUsers(body: any) {
-    const params = getFilterParams(body, this.config.get('LIMIT'));
-    return await User.findAndCountAll(params);
-  }
-
-  async uploadS3(file) {
+  async uploadS3(buffer, name) {
     const params = {
       Bucket: this.config.get('S3_BUCKET'),
-      Key: this.config.get('S3_DIRECTORY')+"hello.jpg",
-      Body: file.buffer,
+      Key: this.config.get('S3_DIRECTORY') + name,
+      Body: buffer,
     };
     const s3 = this.getS3();
-    const res = await new Promise(function(resolve, reject) {
-      s3.upload(params, function(err, data) {
+    const res = await new Promise(function (resolve, reject) {
+      s3.upload(params, function (err, data) {
         if (err) {
           Logger.error(err);
           reject(err.message);
         }
         resolve(data);
-      })
+      });
     });
     return res;
   }
 
-  async upload(file){
+  async getResizedImage(buffer, type) {
+    const image = await sharp(buffer);
+    const metadata = await image.metadata();
+    const width = metadata.width;
+    const height = metadata.height;
+    const newWidth = parseInt(this.config.get(type));
+    const newHeight = Math.floor((height / width) * newWidth);
+    if (width > newWidth || height > newHeight) {
+      return sharp(buffer).resize(newWidth, newHeight).toBuffer();
+    }
+    return buffer;
+  }
 
-  
-    const res = await this.uploadS3(file);
+  async saveToDestination(buffer, fileName, id) {
+    const storage = this.config.get('STORAGE');
+    if (storage === 'S3' || storage === 'BOTH') {
+      this.uploadS3(buffer, `${id}/${fileName}`);
+    }
+  }
 
-    
-    console.log('RES', res);
+  async upload(file) {
+    const sizeBase = file.size;
+    const extension = file.originalname.split('.').pop().toLowerCase();
+    const name = file.originalname;
+    const id = 1;
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      const large = await this.getResizedImage(
+        file.buffer,
+        'IMAGE_WIDTH_LARGE'
+      );
+      const medium = await this.getResizedImage(
+        file.buffer,
+        'IMAGE_WIDTH_MEDIUM'
+      );
+      const small = await this.getResizedImage(
+        file.buffer,
+        'IMAGE_WIDTH_SMALL'
+      );
+      this.saveToDestination(large, `large.${extension}`, id);
+      this.saveToDestination(medium, `medium.${extension}`, id);
+      this.saveToDestination(small, `small.${extension}`, id);
+    } else if (file.mimetype === "'video/mp4'") {
+      this.saveToDestination(file.buffer, `file.${extension}`, id);
+      if (this.config.get('VIDEO_THUMBNAIL') === 'true') {
+        // Generate Thumbnail
+        console.log('generateThumbnail');
+      }
+    } else {
+      this.saveToDestination(file.buffer, `file.${extension}`, id);
+    }
+
+    // const res = await this.uploadS3(file);
+
+    // console.log('RES', res);
   }
 }
